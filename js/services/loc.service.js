10db75e94121ae8d9fa9b40d2a78c8e04ac55110
EdenAlmighty <edengilady1@gmail.com>
2024-03-06 18:21:11 +0200

    a

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -38,7 +38,8 @@ function query() {
         .then(locs => {
             if (gFilterBy.txt && gFilterBy.geo.address) {
                 const regex = new RegExp(gFilterBy.txt, 'i')
-                locs = locs.filter(loc => regex.test(loc.name))
+                locs = locs.filter(loc => regex.test(loc.name) ||
+                (loc => regex.test(loc.geo.address)))
             }
             if (gFilterBy.minRate) {
                 locs = locs.filter(loc => loc.rate >= gFilterBy.minRate)
@@ -54,10 +55,10 @@ function query() {
                 locs.sort((p1, p2) => (p1.rate - p2.rate) * gSortBy.rate)
             } else if (gSortBy.name !== undefined) {
                 locs.sort((p1, p2) => p1.name.localeCompare(p2.name) * gSortBy.name)
-            } else if (gSortBy.created !== undefined){
-                locs.sort((p1,p2) => (p1.rate - p2.rate) * gSortBy.created)
+            } else if (gSortBy.created !== undefined) {
+                locs.sort((p1, p2) => (p1.rate - p2.rate) * gSortBy.created)
             }
-        
+
 
             return locs
         })
